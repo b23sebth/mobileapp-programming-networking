933e3a1f14141cd32a269b6c893a584721f51c2c
b23sebth <b23sebth@student.his.se>
2024-04-29 11:06:51 +0200

    Added mountain java class.

---
+++ app/src/main/java/com/example/networking/mountain.java
@@ -0,0 +1,44 @@
+package com.example.networking;
+
+public class mountain {
+    private String name;
+    private String type;
+    private String location;
+    private int size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public mountain(String name) {
+        this.name = name;
+    }
+}
